f4c5208444a4932ba55de38a613d0e7fdd6a6f40
Pavel Kuzkin <18251378+pakuzkin@users.noreply.github.com>
2023-03-17 13:57:52 -0700

    Part 2. Make faster
    Use pandas library for reading csv and mean calculation
    Improved performance from .4 seconds to 0.08 seconds

--- run.py
+++ run.py
@@ -1,14 +1,5 @@
 def run():
-
-    # Mapping column names to the input data.
-    #  * Column 1 - time
-    #  * Column 2 - humidity
-    #  * Column 3 - salinity
-    #  * Column 4 - air_temperature
-    #  * Column 5 - water_temperature
-    #  * Column 6 - wind_speed
-
-    COLUMN_NAMES = [
+    headers = [
         "time",
         "humidity",
         "salinity",
@@ -16,56 +7,13 @@ def run():
         "water_temperature",
         "wind_speed"
     ]
-
-    # Load data from a local CSV file into a list of lists
-    data = []
-    with open('data.csv') as csvdata:
-        csv_reader = csv.reader(csvdata)
-        for row in csv_reader:
-            data.append(row)
-
-    # Turn all of the data into numerical values
-    # so we can take the average of each column
-    numeric_data = []
-    for row in data:
-        new_row = []
-        # First column is time, which we dont need average for, so skip it.
-        for value in row[1:]:
-            new_row.append(float(value))
-        numeric_data.append(new_row)
-
-    # Organize the data into columns
-    column_2 = []
-    column_3 = []
-    column_4 = []
-    column_5 = []
-    column_6 = []
-    for row in numeric_data:
-        if not math.isnan(row[0]):
-            column_2.append(row[0])
-        if not math.isnan(row[1]):
-            column_3.append(row[1])
-        if not math.isnan(row[2]):
-            column_4.append(row[2])
-        if not math.isnan(row[3]):
-            column_5.append(row[3])
-        if not math.isnan(row[4]):
-            column_6.append(row[4])
-
-    # Calculate the average of each column
-    col_2_avg = sum(column_2) / len(column_2)
-    col_3_avg = sum(column_3) / len(column_3)
-    col_4_avg = sum(column_4) / len(column_4)
-    col_5_avg = sum(column_5) / len(column_5)
-    col_6_avg = sum(column_6) / len(column_6)
-
-    # Return the averages of each column
+    df = pd.read_csv('data.csv', header=None, names=headers)
     return {
-        'humidity': col_2_avg,
-        'salinity': col_3_avg,
-        'air_temperature': col_4_avg,
-        'water_temperature': col_5_avg,
-        'wind_speed': col_6_avg
+        'humidity': df["humidity"].mean(),
+        'salinity': df["salinity"].mean(),
+        'air_temperature': df["air_temperature"].mean(),
+        'water_temperature': df["water_temperature"].mean(),
+        'wind_speed': df["wind_speed"].mean(),
     }
 
 
@@ -73,8 +21,16 @@ if __name__ == '__main__':
     import sys
     import time
     import math
-    import csv
+    import pandas as pd
 
+    '''
+    +---------+--------------------+
+    | Library | Run time (seconds) |
+    +---------+--------------------+
+    | CSV     | 0.407449           |
+    | Pandas  | 0.087758           |
+    +---------+--------------------+
+    '''
     start = time.perf_counter()
     averages = run()
     end = time.perf_counter()
